624d4c3f4d4c1b1364625b5e72d3d0b6a1ae8e72
Daekeun Ko <kdkcom1234@gmail.com>
2024-03-08 14:37:55 +0900

    스타일 코드 분리

--- app/about-us/components/avatar.tsx
+++ app/about-us/components/avatar.tsx
@@ -1,10 +1,12 @@
+import { CSSProperties } from "react"
+
 export default function Avartar() {
-  return <i style={{
+  const avatarStyle: CSSProperties = {
     border: "1px solid black",
     borderRadius: "50%",
     fontStyle: "normal",
-    padding: "0.25rem"
-  }}>
-    🐶
-  </i>
+    padding: "0.25rem",
+  }
+
+  return <i style={avatarStyle}>🐶</i>
 }
