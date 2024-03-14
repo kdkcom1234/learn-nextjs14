dea8cf17e041abcf092f67be57322ab9c93bd2f0
Daekeun Ko <kdkcom1234@gmail.com>
2024-03-14 14:24:51 +0900

    parallel requests

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,31 @@
-import { json } from "stream/consumers";
+import Link from "next/link";
 
 export const metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
   // URL이 변경되지 않으면, 첫번째 호출을 캐싱한 데이터를 사용.
-  // const response = await fetch(URL);
-  const response = await fetch(URL + "?t=" + new Date().getTime());
+  // const response = await fetch(API_URL);
+  // const response = await fetch(API_URL + "?t=" + new Date().getTime());
+
+  // 캐시 사용하지 않음
+  const response = await fetch(API_URL, { cache: "no-store" });
   return response.json();
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
